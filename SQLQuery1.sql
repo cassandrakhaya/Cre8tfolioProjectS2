0c2af8db52fde844a98eb3f01067baa4488e3569
cassandrakhaya <ck825@hotmail.com>
2025-01-16 22:32:18 +0100

    Voortgang Comments

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT Comment ON;
+
+-- Example of inserting data with an explicit identity value
+INSERT INTO Comment (Id, PortfolioPostId, Content, Author)
+VALUES (1, 123, 'This is a comment', 'Jane Doe');
